2cd7c718ff0f1cf0cc4267ab4effb1bbbd38e27f
HaiderIkramMaan <haiderikrammaan@gmail.com>
2026-08-11 04:20:34 +0500

    right now this class 5 problems in package importing

---
+++ src/service/FlightRegistry.java
@@ -0,0 +1,42 @@
+package service;
+//package airline.reservation.service;
+
+import airline.reservation.model.Flight;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+
+public class FlightRegistry {
+
+    private static final FlightRegistry INSTANCE = new FlightRegistry();
+
+    private final Map<String, Flight> flights = new ConcurrentHashMap<>();
+
+    private FlightRegistry() {
+    }
+
+    public static FlightRegistry getInstance() {
+        return INSTANCE;
+    }
+
+    public void register(Flight flight) {
+        flights.put(flight.getFlightId(), flight);
+    }
+
+    public void remove(String flightId) {
+        flights.remove(flightId);
+    }
+
+    public Flight get(String flightId) {
+        return flights.get(flightId);
+    }
+
+    public List<Flight> getAllFlights() {
+        return Collections.unmodifiableList(new ArrayList<>(flights.values()));
+    }
+}
+
